dacee81f867d0a3383c6bade6503fbaa82b683b7
ricardo.ruben <ricgaspar13@gmail.com>
2025-10-18 17:38:18 -0300

    Teste

--- src/Estudante.java
+++ src/Estudante.java
@@ -1,3 +1,6 @@
+
+
 public class Estudante {
-    
+    private int id;
+    private String nomes;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        nomes.add("Ana");
+        nomes.add("Bruno");
+        nomes.add("Carla");
+        nomes.add("Diego");
+        nomes.add("Elisa");
 
     }
 }
