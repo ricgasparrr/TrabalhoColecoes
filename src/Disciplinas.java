9706fbe03fa5b5f410be010d6065f5bd6a523ddb
yuri <yuridouglas45@gmail.com>
2025-10-18 18:22:55 -0300

    implementação da classe discplina (parte B), com a lógica para registrar somente 1 disciplina sem duplicatas.

--- src/Disciplinas.java
+++ src/Disciplinas.java
@@ -1,3 +1,47 @@
 public class Disciplinas {
+    private String codigo;
+    private String nome;
 
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        //verifica se a disciplina ja está registrada na memória
+        if (this == o) return true;
+
+        //se for nulo, então não são iguais. ai registra na memória
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+
+        Disciplinas that = (Disciplinas) o;
+
+        return this.codigo.equals(that.codigo);
+    }
+      @Override
+      public int hashCode() {
+
+
+
+          return codigo.hashCode();
+        }
+        @Override
+    public String toString(){
+        return codigo + " - " + nome;
+    }
 }
